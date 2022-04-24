ccbf099e10583779a5c7dbd09974a26eaea423e9
Dmitry Ghoncharko <dafae722@gmail.com>
2022-04-24 08:15:25 +0300

    first tasks for lesson 1

---
+++ src/lesson1/Task4.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+/**
+ * Напиши программу, которая в методе main объявляет такие переменные:
+ * name типа String, age типа int и city типа String.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
---
+++ src/lesson1/Task9.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+/**
+ * Объяви переменную name типа String
+ * Сразу же в строке объявления присвой ей какое нибудь значение
+ * Выведи на экран переменную name
+ */
+public class Task9 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
